b86c12d3e8223f3f9a7bf3af64e2858cf69b8fe4
Gleb Borisov <glborisov@yandex.ru>
2023-03-28 18:38:14 +0300

    changing back

--- task_1.py
+++ task_1.py
@@ -18,14 +18,5 @@ for one_week_total_price_sum in range(6 * 4):
 print('\nКол-во денег на призы за весь курс обучения -', total_course_sum)
 
 students = int(input('\nВведите кол-во студентов на курсе - '))
-<<<<<<< Updated upstream
 
 print('\nПереплата на каждого студента за курс составит -', total_course_sum / students)
-
-print('\nДобавил изменения после того как отложил предыдущий коммит')
-
-print('Добавил второе отложение')
-
-=======
-print('\nПереплата на каждого студента за курс составит -', total_course_sum / students)
->>>>>>> Stashed changes
